b4b849b13b46f25b0ba3575f6e6c2af28107436e
JoshuaMarGar <asereje007@hotmail.com>
2017-03-10 14:18:47 +0100

    Inicio de proyecto

--- src/creategit/CreateGIT.java
+++ src/creategit/CreateGIT.java
@@ -4,6 +4,7 @@ public class CreateGIT {
 
     public static void main(String[] args) {
         System.out.println("Inicio de proyecto");
+        System.out.println("Inicio de proyecto");
     }
     
 }
